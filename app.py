af5c12294c3fa231ab99dd62d0645c7dc647e2a0
BEAK-SEUNGYUN <qortmddbs7@naver.com>
2021-07-28 15:18:39 +0900

    앱은 포기.. 플라스크 웹을 사용하기로함

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def index():
 
 @ app.route('/webcam')
 def webcam():
-    return Response(yolo_webcam.webcam('http://172.20.10.2:8080/vedio'))
+    return Response(yolo_webcam.webcam('http://192.168.219.122:8080/video'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="192.168.56.1", port="8080", debug=True)
